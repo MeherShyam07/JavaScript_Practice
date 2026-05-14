6dfa271a0cd5c68aa75f4c187062e95b9e12ce7c
MeherShyam07 <meher.shyam07@gmail.com>
2026-05-14 22:05:33 +0530

    updated

---
+++ Data Types/datatypes.js
@@ -0,0 +1,47 @@
+
+console.log("-----Data Types in JavaScript: -----");
+console.log("JavaScript has five primitive data types: String, Number, Boolean, Undefined, and Null.");
+
+console.log("In addition to primitive data types, JavaScript also has a non-primitive data type called Object.");
+console.log("The non-primitive data types include Object, Array, and Function.");
+
+
+console.log("---using String data type: ---");
+//String data type
+let name = "Meher Shyam";
+console.log(name + " is a " + typeof name + " data type.");
+
+let greeting = "Hello Manikonda Congrats on your new job";
+console.log(greeting + " is a " + typeof greeting + " data type.");
+
+console.log("---using Number data type: ---");
+//Number data type
+let age = 30;
+console.log(age + " is a " + typeof age + " data type.");
+
+let accountBalance = 1000.50;
+console.log(accountBalance + " is a " + typeof accountBalance + " data type.");
+
+console.log("---using Boolean data type: ---");
+//Boolean data type
+let isStudent = true;
+console.log(isStudent + " is a " + typeof isStudent + " data type.");
+
+let driverLicense = false;
+console.log(driverLicense + " is a " + typeof driverLicense + " data type.");
+
+console.log("---using Undefined data type: ---");
+//Undefined data type
+let undefinedValue;
+console.log(undefinedValue + " is an " + typeof undefinedValue + " data type.");
+
+let road;
+console.log(road + " is an " + typeof road + " data type.");
+
+console.log("---using Null data type: ---");
+//Null data type
+let nullValue = null;
+console.log(nullValue + " is a " + typeof nullValue + " data type.");
+
+let emptyValue = null;
+console.log(emptyValue + " is a " + typeof emptyValue + " data type.");
---
+++ Variables/variables.js
@@ -0,0 +1,63 @@
+
+console.log("---Using var keyword for variable declaration and initialization: ---");
+
+//Single statement variable declaration and initialization
+var x = 20 
+
+//multiple line variable declaration
+var y;   //variable declaration
+y = 10;  //variable initialization
+var z = x + y;
+console.log(z);
+
+//Single line multiple variable declaration and initialization
+var xx=100, y=200, z=300;
+console.log(xx);
+console.log(y);
+console.log(z);
+console.log(xx+y+z);
+console.log(xx*y*z);
+console.log(xx/y);
+console.log(xx-y);
+console.log(xx%y);
+
+
+console.log("---Using let keyword for variable declaration and initialization: ---");
+
+
+//Multiple line variable declaration and initialization
+let c;
+c = 200;
+console.log(c);
+
+//Single line variable declaration and initialization
+let ge = "Hello World";
+console.log(ge); 
+
+//Multiple line variable declaration and initialization
+let i = "Hello"; b = "World"; j = 1000;
+console.log(i);
+console.log(b);
+console.log(j);
+
+console.log("---Difference between var and let: ---");
+var h;
+console.log(h); //undefined
+var k = 10;
+console.log(h); //10
+let f;
+console.log(f); // Error: Cannot access 'f' before initialization
+let l = 20;
+console.log(l); //20
+
+
+console.log("---Using const keyword for variable declaration and initialization: ---");
+//Single line variable declaration and initialization
+const PI = 3.14;
+console.log(PI);
+//const cannot be re-assigned and re-declared
+//PI = 3.14159; // Error: Assignment to constant variable.
+
+
+
+
